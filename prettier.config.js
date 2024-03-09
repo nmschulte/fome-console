fa7a22baa6254c7eff3829b2b25417565f1ca83b
Nathan Schulte <nmschulte@desmas.net>
2024-03-09 09:55:14 -0600

    use ESM for Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+var config = {
+  $schema: "https://json.schemastore.org/prettierrc",
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 120,
+  trailingComma: "all"
+};
+
+export default config;
