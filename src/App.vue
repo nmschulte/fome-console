0cbc6d50e48fb2a1422f04bf970b1984983010d1
Nathan Schulte <nmschulte@desmas.net>
2024-03-09 10:04:25 -0600

    FOME-ize

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="FOME logo" class="logo" src="@/assets/logo-transparent.png" width="168" height="60" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
